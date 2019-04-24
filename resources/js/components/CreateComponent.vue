cdc844d917e8d713c4a2e0afce8d0b6b22ee8058
yong <yong@asial.co.jp>
2019-04-24 23:24:49 +0900

    add db function

--- resources/js/components/CreateComponent.vue
+++ resources/js/components/CreateComponent.vue
@@ -34,7 +34,10 @@
     },
     methods: {
       addPost(){
-        console.log(this.post);
+        let uri = 'http://127.0.0.1:8000/api/post/create';
+        this.axios.post(uri, this.post).then((response) => {
+          this.$router.push({name: 'posts'});
+        });
       }
     }
   }
